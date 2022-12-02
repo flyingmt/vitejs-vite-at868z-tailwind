90863462db7de4a53b6464e96252580745329397
flyingmt <flyingmt@gmail.com>
2022-12-02 19:36:31 +0900

    3 files modified

--- src/components/TodoList.tsx
+++ src/components/TodoList.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import { RootState } from '../redux';
-import { Task } from '../redux/tasksSlice';
+import { Task, getTask } from '../redux/tasksSlice';
 import TodoItem from './TodoItem';
+import { useDispatch } from 'react-redux';
 
 const TodoList = () => {
+  // const dispatch = useDispatch();
+  // dispatch(getTask());
+
   const todos = useSelector((state: RootState) => {
     return state.tasks;
   });
@@ -12,7 +16,13 @@ const TodoList = () => {
   return (
     <div id="tasks" className="my-5">
       {todos.map((todo: Task) => (
-        <TodoItem key={todo.id} id={todo.id} title={todo.title} />
+        <TodoItem
+          key={todo.id}
+          id={todo.id}
+          title={todo.title}
+          userId={todo.userId}
+          completed={todo.completed}
+        />
       ))}
     </div>
   );
--- src/redux/tasksSlice.tsx
+++ src/redux/tasksSlice.tsx
@@ -1,4 +1,5 @@
 import { createSlice, PayloadAction } from '@reduxjs/toolkit';
+import axios from 'axios';
 
 export interface Task {
   userId: number;
@@ -14,6 +15,17 @@ export const tasksSlice = createSlice({
   name: 'tasks',
   initialState: initialState,
   reducers: {
+    getTask: (state) => {
+      axios
+        .get<Task[]>('https://jsonplaceholder.typicode.com/todos', {
+          headers: {
+            'Content-Type': 'application/json',
+          },
+        })
+        .then((response) => {
+          state.value = response.data;
+        });
+    },
     addTask: (state, action: PayloadAction<Task>) => {
       const newTask: Task = {
         userId: 0,
@@ -29,6 +41,6 @@ export const tasksSlice = createSlice({
   },
 });
 
-export const { addTask, deleteTask } = tasksSlice.actions;
+export const { getTask, addTask, deleteTask } = tasksSlice.actions;
 
 export default tasksSlice.reducer;
